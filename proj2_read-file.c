13b8034d2781b272932081e5dedbe5723833664c
jethro34 <jh.triana@hotmail.com>
2023-10-12 19:33:05 -0700

    final 2.0

--- proj2_read-file.c
+++ proj2_read-file.c
@@ -11,7 +11,7 @@ int main(void)
   // read name of file
   char filename[100];
   printf("Enter name of file to be read: ");
-  scanf("%s", filename);
+  scanf("%99s", filename);
   getchar();
   printf("\n");
 
@@ -26,10 +26,18 @@ int main(void)
 
   // allocate buffer to read contents in
   char* readBuffer = malloc(256 * sizeof(char));
+  if (readBuffer == NULL) {
+    perror("malloc() failed");
+    close(fd);
+    exit(1);
+  }
+
   // read & store number of characters read
   int bytes_read = read(fd, readBuffer, 256);
   if (bytes_read == -1) {
-    printf("read() failed.");
+    perror("read() failed.");
+    free(readBuffer);
+    close(fd);
     exit(1);
   }
   printf("read() successful\n");
@@ -42,6 +50,7 @@ int main(void)
 
   // add null-terminator character
   readBuffer[strlen(readBuffer)-1] = '\0';
+  // also possible: readBuffer[bytes_read] = '\0';
 
   /* // check readBuffer after appending null-terminator
   for (int i=0; i<=strlen(readBuffer); i++)
@@ -49,10 +58,12 @@ int main(void)
   printf("\n");
   */
 
-  // print out contents
-  printf("Contents read from \"%s\":\n%s\n", filename, readBuffer);
-
-  close(fd);
+  // print out contents if data was read
+  if (bytes_read > 0)
+    printf("Contents read from \"%s\":\n%s\n", filename, readBuffer);
+  else printf("No data read from \"%s\"\n", filename);
+  
   free(readBuffer);
+  close(fd);
   return 0;
 }
