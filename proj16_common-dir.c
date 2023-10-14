50fdb7ff096eef5b020dcbe5e9c7700cd3bf07c2
jethro34 <jh.triana@hotmail.com>
2023-10-14 13:55:01 -0700

    declare strtok_r vars before loop

--- proj16_common-dir.c
+++ proj16_common-dir.c
@@ -12,21 +12,22 @@ int main(void)
     "/home/me/user/temp/c/d"
   };
   char* marker[3] = { NULL, NULL, NULL };
+  char* tempdirPtr = NULL;
+  char* tempdirPtr2 = NULL;
   char commonpath[1024] = "/";
   int first = 1;
 
   for (;;) {
-    int i = 0;
-    char* tempdirPtr = strtok_r((first ? paths[i] : NULL), "/", &marker[i]); 
-    for (i=1; i<3; i++) {
-      char* tempdirPtr2 = strtok_r((first ? paths[i] : NULL), "/", &marker[i]);
+    tempdirPtr = strtok_r((first ? paths[0] : NULL), "/", &marker[0]); 
+    for (int i=1; i<3; i++) {
+      tempdirPtr2 = strtok_r((first ? paths[i] : NULL), "/", &marker[i]);
       if (strcmp(tempdirPtr, tempdirPtr2) != 0) {
         printf("common tree is: \"%s\"\n", commonpath);
         return 0;
       }
-      strcat(commonpath, "/");
-      strcat(commonpath, tempdirPtr);
     }
+    strcat(commonpath, "/");
+    strcat(commonpath, tempdirPtr);
     first = 0;
   }
   return 0;
